df2b68164c65854b386b0696f46c3522eaf657e9
luizlcezario <luizlcezario@gmail.com>
2024-05-22 20:41:26 -0300

    Revert "final"
    This reverts commit 30dbacb861405fa97715f57e3828ef3ad66158b5.

--- frontend/src/components/soundControl/index.jsx
+++ frontend/src/components/soundControl/index.jsx
@@ -17,7 +17,7 @@ const SoundControl = ({ audioSrc }) => {
   return (
     <div className="sound-icon" onClick={toggleSound}>
       <img src={isPlaying ? 'https://img.icons8.com/ios-glyphs/30/00ffea/musical-notes.png' : 'https://img.icons8.com/ios-glyphs/30/00ffea/mute.png'} alt="Sound Icon" />
-      <audio id="background-music" loop>
+      <audio id="background-music" loop autoPlay>
         <source src={audioSrc} type="audio/mpeg" />
         Seu navegador não suporta o elemento de áudio.
       </audio>
