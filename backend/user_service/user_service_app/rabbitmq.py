b1e9d77e5ea97898497e223cbac4b3f22001b9d9
lcezario_meli <luiz.cezario@mercadolivre.com>
2024-05-30 15:42:35 -0300

    docker_fail_on_mac_luiz

--- backend/user_service/user_service_app/rabbitmq.py
+++ backend/user_service/user_service_app/rabbitmq.py
@@ -1,6 +1,5 @@
 import pika
 import os
-import
 
 rabbitmq_host = os.getenv('RABBITMQ_HOST', 'rabbitmq')
 rabbitmq_user = os.getenv('RABBITMQ_USER', 'guest')
@@ -10,11 +9,6 @@ try:
     connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq", 5672, '/', credentials))
     channel = connection.channel()
     print("ok test rabbitmq connected")
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='CREDENTIALS_TO_AUTHENTICATE', on_message_callback=on_request)
-    print(" [x] Awaiting RPC requests")
-    channel.start_consuming()
 
 except Exception as e:
     print(e)
